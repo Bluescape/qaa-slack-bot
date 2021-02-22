207fdc5c147d800430f8a4ddb9a1abe26822b7bf
Fiona <fiona.hu@bluescape.com>
2021-02-22 11:11:16 -0500

    Added some basic imports to index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('Hello world!')
+const core = require("@actions/core");
+const github = require("@actions/github");
+const testrail = require("testrail-api");
+
+const main = async () => {
+  console.log("Hello world")
+}
+
+main().catch((err) => core.setFailed(err.message));
