da47a229b726fe74536e06acc3d6b5d3864467dd
Fiona <fiona.hu@bluescape.com>
2021-02-26 15:23:45 -0500

    Testing some new lines; fixed lint

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ const main = async () => {
   testText.push(makeTestLine('Branch', ghBranch))
   if (ghPackage) testText.push(makeTestLine('Package', ghPackage))
   if (runStatus) testText.push(makeTestLine('Status', runStatus))
-  if (extraMarkdownText) testText.push(extraMarkdownText);
+  if (extraMarkdownText) testText.push(extraMarkdownText)
 
   const blocks = []
   blocks.push(makeButtonBlock('Github Run', `${ghRepoLink}/actions/runs/${ghRunId}`, 'primary'))
