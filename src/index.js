29684ee58d941f8841cd5dcd3b58a61aab9952f0
Fiona Hu <76963786+fionahu-bluescape@users.noreply.github.com>
2021-03-01 09:52:24 -0500

    QAA-2386: Add Markdown text block possibility (#9)
    * QAA-2386: Add Markdown text block possibility
    
    * Testing some new lines; fixed lint
    
    * Added newlines before and after extras
    
    * Added extra newline
    
    * Trying with dividers
    
    * Refactor to make it easier to add stuff
    
    * Testing without the extra text
    
    * Updated linting
    
    * Removede comments

--- src/index.js
+++ src/index.js
@@ -9,11 +9,13 @@ const main = async () => {
   const runStatus = core.getInput('run_status') || undefined
   const testrailProjectId = core.getInput('testrail_project_id') || undefined
   const ghPackage = core.getInput('package') || undefined
+  const extraMarkdownText = core.getInput('extra_text') || undefined
 
   const context = github.context
   const ghRunId = context.runId
   const ghRepoName = context.repo.repo
-  const ghBranch = _.get(context, ['event', 'branch']) || _.get(context, ['ref'])
+  const ghBranch =
+    _.get(context, ['event', 'branch']) || _.get(context, ['ref'])
   const ghRepoLink = context.payload.repository.html_url
   const webhook = new IncomingWebhook(webhookUrl)
 
@@ -24,33 +26,60 @@ const main = async () => {
   if (ghPackage) testText.push(makeTestLine('Package', ghPackage))
   if (runStatus) testText.push(makeTestLine('Status', runStatus))
 
-  const blocks = []
-  blocks.push(makeButtonBlock('Github Run', `${ghRepoLink}/actions/runs/${ghRunId}`, 'primary'))
-  if (bluescapeUrl) blocks.push(makeButtonBlock('Bluescape Environment', `https://client.${bluescapeUrl}/my`))
-  blocks.push(makeButtonBlock('Repository', ghRepoLink))
-  if (testrailProjectId) blocks.push(makeButtonBlock('Testrail Project', `https://testrail.bluescape.com/index.php?/projects/overview/${testrailProjectId}`))
-
-  const slackMessage = {
-    blocks: [
-      {
-        type: 'divider'
-      },
-      {
-        type: 'section',
-        text: {
-          type: 'mrkdwn',
-          text: testText.join('\n')
-        }
-      },
-      {
-        type: 'actions',
-        elements: blocks
-      },
-      {
-        type: 'divider'
-      }
-    ]
+  const links = []
+  links.push(
+    makeButtonBlock(
+      'Github Run',
+      `${ghRepoLink}/actions/runs/${ghRunId}`,
+      'primary'
+    )
+  )
+  if (bluescapeUrl) {
+    links.push(
+      makeButtonBlock(
+        'Bluescape Environment',
+        `https://client.${bluescapeUrl}/my`
+      )
+    )
   }
+  links.push(makeButtonBlock('Repository', ghRepoLink))
+  if (testrailProjectId) {
+    links.push(
+      makeButtonBlock(
+        'Testrail Project',
+        `https://testrail.bluescape.com/index.php?/projects/overview/${testrailProjectId}`
+      )
+    )
+  }
+
+  const divider = { type: 'divider' }
+  const slackMessage = {
+    blocks: []
+  }
+  slackMessage.blocks.push(divider)
+  slackMessage.blocks.push({
+    type: 'section',
+    text: {
+      type: 'mrkdwn',
+      text: testText.join('\n')
+    }
+  })
+  if (extraMarkdownText) {
+    slackMessage.blocks.push(divider)
+    slackMessage.blocks.push({
+      type: 'section',
+      text: {
+        type: 'mrkdwn',
+        text: extraMarkdownText
+      }
+    })
+    slackMessage.blocks.push(divider)
+  }
+  slackMessage.blocks.push({
+    type: 'actions',
+    elements: links
+  })
+  slackMessage.blocks.push(divider)
   await webhook.send(slackMessage)
 }
 
